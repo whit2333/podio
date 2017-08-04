42c4ef7aa1cf41bd78038724b6b7efcadc732890
Whitney Armstrong <warmstrong@anl.gov>
2017-08-04 14:53:31 -0500

    Working on legacy2lcio2 converter.
    2 lcio 2 furious!!!
    
    	modified:   lcio2_tools/src/legacy2lcio2.cc

--- lcio2_tools/src/legacy2lcio2.cc
+++ lcio2_tools/src/legacy2lcio2.cc
@@ -1,10 +1,45 @@
-#include "lcio2/MCParticle.h"
 #include "lcio2/EventInfoCollection.h"
-//#include "lcio2/JetCollection.h"
-//
+#include "lcio2/MCParticleCollection.h"
+#include "lcio2/MCParticle.h"
 
 #include "lcio.h"
 #include <stdio.h>
+#include <typeinfo>
+
+#include "UTIL/LCTOOLS.h"
+#include "UTIL/Operators.h"
+#include "UTIL/LCObjectHandle.h"
+#include "UTIL/LCTime.h"
+#include "UTIL/CellIDDecoder.h"
+#include "UTIL/PIDHandler.h"
+
+#include "EVENT/LCCollection.h"
+#include "EVENT/SimCalorimeterHit.h"
+#include "EVENT/CalorimeterHit.h"
+#include "EVENT/RawCalorimeterHit.h"
+#include "EVENT/SimTrackerHit.h"
+#include "EVENT/TrackerHit.h"
+#include "EVENT/TrackerHitPlane.h"
+#include "EVENT/TrackerHitZCylinder.h"
+#include "EVENT/TPCHit.h"
+#include "EVENT/TrackerRawData.h"
+#include "EVENT/TrackerData.h"
+#include "EVENT/TrackerPulse.h"
+#include "EVENT/LCIO.h"
+#include "EVENT/MCParticle.h"
+#include "EVENT/LCFloatVec.h"
+#include "EVENT/LCIntVec.h"
+#include "EVENT/Track.h"
+#include "EVENT/Cluster.h"
+#include "EVENT/ReconstructedParticle.h"
+#include "EVENT/Vertex.h"
+#include "EVENT/LCGenericObject.h"
+#include "EVENT/LCRelation.h"
+
+//#include "IMPL/LCFlagImpl.h"
+#include "UTIL/BitSet32.h"
+#include "LCIOSTLTypes.h"
+
 
 #include "IO/LCReader.h"
 #include "IMPL/LCTOOLS.h"
@@ -35,169 +70,138 @@
 // podio specific includes
 #include "podio/EventStore.h"
 #include "podio/ROOTReader.h"
+#include "podio/ROOTWriter.h"
 
-void writeEventTree(const char* FILEN = "legacy_reconstruction_datafile.slcio") {
-    
+//template<class T>
+//void setMomentum(T& p, double pt, double eta, double phi) {
+//    p.Px = TMath::Cos(phi)*pt;
+//    p.Py = TMath::Sin(phi)*pt;
+//    p.Pz = TMath::Sinh(eta)*pt;
+//}
+//// First get the collection (note that this will work differently in FCCSW, see below)
+//auto& myParticleColl = store.create<fcc::ParticleCollection>("recoParticles");
+//auto& myMCParticleCollection = store.create<fcc::MCParticleCollection>("genParticles");
+//auto particle = myParticleCollection.create();
+//auto mcParticle = myMCParticleCollection.create();
+//setKinematics(particle.Core, 20, 0.1, 0.5);
+//setKinematics(mcParticle.Core, 20, 0.1, 0.5);
+
+void writeEventTree(podio::EventStore& store,
+                    bool verbose,
+                    podio::ROOTWriter& writer,
+                    char* FILEN = "legacy_reconstruction_datafile.slcio")
+{
+
   //--- create a ROOT file, a tree and a branch ...
-  
-  TFile* file = new TFile( "lcioEventTree.root" , "RECREATE");    
-  
-  TTree* tree = new TTree( "LCIO" , "lcio event data tree");
-  
-  std::string treeName("LCEvent") ;
-  
+
+  //TFile* file = new TFile( "lcioEventTree.root" , "RECREATE");    
+  //TTree* tree = new TTree( "LCIO" , "lcio event data tree");
+  //std::string treeName("LCEvent") ;
   IMPL::LCEventImpl* treeEvt=0 ;
+  //std::string type("IMPL::LCEventImpl") ;
+  //TBranch* mcpBranch = tree->Branch( treeName.c_str(), 
+  //      			     type.c_str(), 
+  //      			     (void*) &treeEvt, 
+  //      			     1024, // record size 
+  //      			     199    // split level 
+  //      			     );
+  //
+  //std::cout << " loaded LCIO library and dictionary ... " << std::endl ;
 
-  std::string type("IMPL::LCEventImpl") ;
-  
-  TBranch* mcpBranch = tree->Branch( treeName.c_str(), 
-				     type.c_str(), 
-				     (void*) &treeEvt, 
-				     1024, // record size 
-				     199    // split level 
-				     );
-  
-
-  std::cout << " loaded LCIO library and dictionary ... " << std::endl ;
-  
   int nEvents = 0  ;
-
   int maxEvt = 10000 ; 
-
   IO::LCReader* lcReader = IOIMPL::LCFactory::getInstance()->createLCReader() ;
-
   lcReader->open( FILEN ) ;
-  
+
+  auto& mcps = store.create<lcio2::MCParticleCollection>("MCParticle");
+  writer.registerForWrite("MCParticle");
 
   //----------- the event loop -----------
   EVENT::LCEvent* evt = 0 ;
   while( (evt = lcReader->readNextEvent()) != 0  && nEvents < maxEvt ) {
-    
-    if( nEvents < 3 )  // only dump first 3 events
+
+    if( nEvents < 1 ){  // only dump first event
       UTIL::LCTOOLS::dumpEvent( evt ) ;
+    }
+
+    int  run_number   = evt->getRunNumber();
+    int  event_number = evt->getEventNumber();
+    auto collections  = evt->getCollectionNames();
+
+    for(const auto& v : (*collections) ) {
+
+      // MCParticle
+      auto a_collection = evt->getCollection(v);
+      if( a_collection->getTypeName() == std::string("MCParticle") ) {
+
+        int n_elem = a_collection->getNumberOfElements();
+        for(int i_elem = 0; i_elem<n_elem; i_elem++) {
+
+          auto part =  dynamic_cast<MCParticle*>( a_collection->getElementAt( i_elem ) ) ;
+          auto a_mcp = lcio2::MCParticle();
+          a_mcp.momentum({part->getMomentum()[0] ,  part->getMomentum()[1] ,   part->getMomentum()[2] });
+          a_mcp.vertex({part->getVertex()[0] ,  part->getVertex()[1] ,   part->getVertex()[2] });
+          a_mcp.pdg( part->getPDG() );
+          a_mcp.genstatus( part->getGeneratorStatus() );
+          a_mcp.mass( part->getMass() );
+          a_mcp.charge( part->getCharge() );
+          a_mcp.time( part->getTime() );
+          a_mcp.endpoint({part->getEndpoint()[0] ,  part->getEndpoint()[1] ,   part->getEndpoint()[2] });
+          mcps.push_back(a_mcp);
+          std::cout << a_mcp << std::endl;
+
+          //printf("[%8.8d]", part->id() );
+          //printf("%5d|"   , i_elem );
+          //printf("%10d|" , part->getPDG() );
+          //printf("% 1.2e,% 1.2e,% 1.2e|" , 
+          //       part->getMomentum()[0] ,
+          //       part->getMomentum()[1] , 
+          //       part->getMomentum()[2] );
+          //printf("% 1.2e,% 1.2e,% 1.2e|" , 
+          //       part->getMomentumAtEndpoint()[0] ,
+          //       part->getMomentumAtEndpoint()[1] , 
+          //       part->getMomentumAtEndpoint()[2] );
+          //printf("% 1.2e \n" , part->getEnergy() ) ; 
+        
+        }
+      }
+    }
 
     nEvents ++ ;
 
     treeEvt = (IMPL::LCEventImpl*) evt ;
-    
 
-    tree->Fill() ;
-
+    //tree->Fill() ;
+    writer.writeEvent();
+    store.clearCollections();
   }
   // -------- end of event loop -----------
 
-  file->Write() ;
-  file->Close() ;
-  
-  delete file ;
-
+  //file->Write() ;
+  //file->Close() ;
+  //delete file ;
 
   std::cout << std::endl 
-	    <<  "  " <<  nEvents 
-	    << " events read from file: " 
-	    << FILEN << std::endl  ;
-  
-  
+    <<  "  " <<  nEvents 
+    << " events read from file: " 
+    << FILEN << std::endl  ;
+
   lcReader->close() ;
-  
   delete lcReader ;
 }
 
 void processEvent(podio::EventStore& store, bool verbose,
                   podio::ROOTReader& reader) {
 
-  // read event information
-  //const lcio2::EventInfoCollection* evinfocoll(nullptr);
-  //bool evinfo_available = store.get("EventInfo", evinfocoll);
-  //if(evinfo_available) {
-  //  auto evinfo = evinfocoll->at(0);
-
-  //  if(verbose)
-  //    std::cout << "event number " << evinfo.number() << std::endl;
-  //}
-
-  //// the following is commented out to test on-demand reading through Jet-Particle association,
-  //// see below
-  //// // read particles
-  //// ParticleCollection* ptcs(nullptr);
-  //// bool particles_available = store.get("GenParticle",ptcs);
-  //// if (particles_available){
-  ////   for(const auto& part : *ptcs) {
-  ////     std::cout<<part.containerID()<<" "<<part.index()<<std::endl;
-  ////   }
-  //// }
-
-  //// read jets
-  //const lcio2::JetCollection* jrefs(nullptr);
-  //bool jets_available = store.get("GenJet",jrefs);
-  //std::vector<lcio2::ConstParticle> injets;
-
-  //if (jets_available){
-  //  if(verbose) {
-  //    reader.getCollectionIDTable()->print();
-  //    std::cout << "jet collection:" << std::endl;
-  //  }
-  //  for(const auto& jet : *jrefs){
-  //    TLorentzVector lv = utils::lvFromPOD(jet.core().p4);
-  //    if(verbose)
-  //      std::cout << "\tjet: E=" << lv.E() << " "<<lv.Eta()<<" "<<lv.Phi()
-  //                <<" npart="<<jet.particles_size()<<std::endl;
-  //    for(auto part = jet.particles_begin(); part != jet.particles_end(); ++part) {
-  //      if(part->isAvailable()) {
-  //        if(verbose)
-  //          std::cout<<"\t\tassociated "<<part->core()<<std::endl;
-  //        injets.push_back(*part);
-  //      }
-  //    }
-  //  }
-  //}
-
-  //// read particles
-  //const lcio2::ParticleCollection* ptcs(nullptr);
-  //bool particles_available = store.get("GenParticle", ptcs);
-  //if (particles_available){
-  //  std::vector<lcio2::Particle> muons;
-  //  // there is probably a smarter way to get a vector from collection?
-
-  //  if(verbose)
-  //    std::cout << "particle collection:" << std::endl;
-  //  for(const auto& ptc : *ptcs){
-  //    if(verbose)
-  //      std::cout<<"\t"<<ptc<<std::endl;
-  //    if( ptc.core().pdgId == 4 ) {
-  //      muons.push_back(ptc);
-  //    }
-  //  }
-  //  // listing particles that are not used in a jet
-  //  std::vector<lcio2::Particle> unused = utils::unused(*ptcs, injets);
-  //  if(verbose)
-  //    std::cout<<"unused particles: "<<unused.size()<<"/"<<ptcs->size()<<" "<<injets.size()<<std::endl;
-
-  //  // computing isolation for first muon
-  //  if(not muons.empty()) {
-  //    const lcio2::Particle& muon = muons[0];
-  //    float dRMax = 0.5;
-  //    const std::vector<lcio2::Particle> incone = utils::inCone( muon.core().p4,
-  //                                                        *ptcs,
-  //                                                        dRMax);
-  //    float sumpt = utils::sumPt(incone);
-  //    if( verbose ) {
-  //      std::cout<<"muon: "<<muon<<" sumpt "<<sumpt<<std::endl;
-  //      std::cout<<"\tparticles in cone:"<<std::endl;
-  //    }
-  //    for(const auto& ptc : incone) {
-  //      if( verbose )
-  //        std::cout<<"\t"<<ptc<<std::endl;
-  //    }
-  //  }
-  //}
 }
 
 
 int main(){
-  auto reader = podio::ROOTReader();
+
+  //auto reader = podio::ROOTReader();
   auto store = podio::EventStore();
-  lcio2::MCParticle part;
+  auto writer = podio::ROOTWriter("example.root", &store);
+  //lcio2::MCParticle part;
   //try {
   //  reader.openFile("example.root");
   //}
@@ -223,6 +227,9 @@ int main(){
   //  reader.endOfEvent();
   //}
   //
-  writeEventTree();
+  writeEventTree(store, 0, writer);
+
+
+writer.finish();
   return 0;
 }
