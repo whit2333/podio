233dccbc6cec94c33971164f2e2ed4d7f62b8a09
Whitney Armstrong <warmstrong@anl.gov>
2017-08-06 21:03:32 -0500

    modified:   ../src/lcio2legacy.cc

--- lcio2_tools/src/lcio2legacy.cc
+++ lcio2_tools/src/lcio2legacy.cc
@@ -1,3 +1,8 @@
+/** @file lcio2legacy
+ *
+ */
+
+//lcio2
 #include "lcio2/EventInfoCollection.h"
 #include "lcio2/MCParticleCollection.h"
 #include "lcio2/MCParticle.h"
@@ -17,10 +22,15 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include "getopt.h"
 
-
+// legacy-lcio
 #include "lcio.h"
 #include "IO/LCWriter.h"
+#include "IO/LCReader.h"
 
 #include "UTIL/LCTOOLS.h"
 #include "UTIL/Operators.h"
@@ -28,6 +38,8 @@
 #include "UTIL/LCTime.h"
 #include "UTIL/CellIDDecoder.h"
 #include "UTIL/PIDHandler.h"
+#include "UTIL/BitSet32.h"
+#include "LCIOSTLTypes.h"
 
 #include "IMPL/LCEventImpl.h" 
 #include "IMPL/LCRunHeaderImpl.h" 
@@ -35,7 +47,6 @@
 #include "IMPL/SimCalorimeterHitImpl.h"
 #include "IMPL/SimTrackerHitImpl.h"
 #include "IMPL/MCParticleImpl.h" 
-//#include "IMPL/LCFlagImpl.h" 
 #include "IMPL/LCTOOLS.h"
 
 #include "EVENT/LCCollection.h"
@@ -61,25 +72,7 @@
 #include "EVENT/Vertex.h"
 #include "EVENT/LCGenericObject.h"
 #include "EVENT/LCRelation.h"
-
-//#include "IMPL/LCFlagImpl.h"
-#include "UTIL/BitSet32.h"
-#include "LCIOSTLTypes.h"
-
-#include "IO/LCReader.h"
-#include "IMPL/LCTOOLS.h"
 #include "EVENT/LCRunHeader.h" 
-
-//#include "EVENT/SimCalorimeterHit.h" 
-#include "EVENT/CalorimeterHit.h" 
-//#include "EVENT/RawCalorimeterHit.h" 
-#include "EVENT/ReconstructedParticle.h"
-//#include "UTIL/CellIDDecoder.h"
-#include "UTIL/Operators.h"
-#include "UTIL/LCIterator.h"
-// Utility functions
-//#include "utilities/VectorUtils.h"
-//#include "utilities/ParticleUtils.h"
 
 // ROOT
 #include "TBranch.h"
@@ -88,14 +81,16 @@
 #include "TROOT.h"
 #include "TLorentzVector.h"
 
-// STL
-#include <vector>
-#include <iostream>
-
 // podio specific includes
 #include "podio/EventStore.h"
 #include "podio/ROOTReader.h"
 #include "podio/ROOTWriter.h"
+
+std::string  exec(const char* cmd);
+bool         fexists(const std::string& filename);
+void         check_field_maps();
+void         download_field_maps();
+void         print_help();
 
 void processToLegacy(podio::EventStore& store,
                      LCEventImpl* evt,
@@ -103,7 +98,6 @@ void processToLegacy(podio::EventStore& store,
                      int rn,
                      int eventNumber)
 {
-
 //BeamCalHits                   SimCalorimeterHit              695 
 //CalorimeterHitRelations       LCRelation                     589
 //EM_BARREL                     CalorimeterHit                   4            
@@ -146,8 +140,8 @@ void processToLegacy(podio::EventStore& store,
   auto& SiVertexEndcapHits  = store.get<lcio2::SimTrackerHitCollection>("SiVertexEndcapHits");
   auto& Tracks = store.get<lcio2::TrackCollection>("Tracks");
   auto& ReconClusters = store.get<lcio2::ClusterCollection>("ReconClusters");
-  auto& EM_BARREL = store.get<lcio2::CalorimeterHitCollection>("EM_BARREL");
-  auto& EM_ENDCAP = store.get<lcio2::CalorimeterHitCollection>("EM_ENDCAP");
+  auto& EM_BARREL  = store.get<lcio2::CalorimeterHitCollection>("EM_BARREL");
+  auto& EM_ENDCAP  = store.get<lcio2::CalorimeterHitCollection>("EM_ENDCAP");
   auto& HAD_BARREL = store.get<lcio2::CalorimeterHitCollection>("HAD_BARREL");
   auto& HAD_ENDCAP = store.get<lcio2::CalorimeterHitCollection>("HAD_ENDCAP");
   auto& PandoraPFOCollection = store.get<lcio2::ReconstructedParticleCollection>("PandoraPFOCollection");
@@ -155,15 +149,13 @@ void processToLegacy(podio::EventStore& store,
   //auto& VXD_RawTrackerHits = store.get<lcio2::TrackerRawDataCollection>("VXD_RawTrackerHits");
   auto& HelicalTrackHits = store.get<lcio2::TrackerHitCollection>("HelicalTrackHits");
 
-
   // create and add some mc particles 
-  LCCollectionVec* mcVec = new LCCollectionVec( LCIO::MCPARTICLE )  ;
-
-  // debug only - don't write MCParticles to output file:
-  // mcVec->setTransient() ;
-
-  // debug only - add the same particle to more than one collection
-  //LCCollectionVec* mcVec2 = new LCCollectionVec( LCIO::MCPARTICLE )  ;
+  LCCollectionVec* mcVec = new LCCollectionVec( LCIO::MCPARTICLE );
+  LCCollectionVec* SiTrackerBarrelHits_leg  = new LCCollectionVec( LCIO::SIMTRACKERHIT );
+  LCCollectionVec* SiTrackerEndcapHits_leg  = new LCCollectionVec( LCIO::SIMTRACKERHIT );
+  LCCollectionVec* SiTrackerForwardHits_leg = new LCCollectionVec( LCIO::SIMTRACKERHIT );
+  LCCollectionVec* SiVertexBarrelHits_leg   = new LCCollectionVec( LCIO::SIMTRACKERHIT );
+  LCCollectionVec* SiVertexEndcapHits_leg   = new LCCollectionVec( LCIO::SIMTRACKERHIT );
 
   if( mcps.isValid() ){
     //-------- print relations for debugging:
@@ -251,7 +243,100 @@ void processToLegacy(podio::EventStore& store,
 }
 
 
-int main(){
+int main(int argc,char** argv) {
+
+   int          run_number        = 0;
+   int          number_of_events  = -1;
+   int          event_start_offset = 0;
+   std::string  input_file_name   = "";
+   std::string  output_file_name  = "";
+   std::string  output_tree_name  = "";
+   std::string  output_dir        = "";
+   std::string  theRest           = "";
+
+   //---------------------------------------------------------------------------
+
+   int index = 0;
+   int iarg  = 0;
+   opterr    = 1; //turn off getopt error message
+   const struct option longopts[] =
+   {
+      {"run",         required_argument,  0, 'r'},
+      {"input",       required_argument,  0, 'i'},
+      {"output",      required_argument,  0, 'o'},
+      {"dir",         required_argument,  0, 'D'},
+      {"treename",    required_argument,  0, 't'},
+      {"events",      required_argument,  0, 'n'},
+      {"offset",      required_argument,  0, 'O'},
+      {"help",        no_argument,        0, 'h'},
+      {0,0,0,0}
+   };
+   while(iarg != -1) {
+      iarg = getopt_long(argc, argv, "o:t:D:r:i:O:n:h", longopts, &index);
+
+      switch (iarg)
+      {
+         case 'i':
+            input_file_name = optarg;
+            break;
+
+         case 'r':
+            run_number = atoi( optarg );
+            break;
+
+         case 'n':
+            number_of_events = atoi( optarg );
+            break;
+
+         case 'O':
+            event_start_offset = atoi( optarg );
+            if(event_start_offset < 0 ) {
+               std::cout << "Error: -O, --offset is negative! Aborting.\n";
+               std::exit(EXIT_FAILURE);
+            }
+            break;
+
+         case 't':
+            output_tree_name = optarg;
+            break;
+
+         case 'D':
+            output_dir = optarg;
+            break;
+
+         case 'o':
+            output_file_name = optarg;
+            if( fexists(output_file_name) ) {
+               std::cout << "Error : " << output_file_name << " already exist"  << std::endl;
+               exit(EXIT_FAILURE);
+            }
+            break;
+
+         case 'h':
+            print_help();
+            exit(0);
+            break;
+
+         case '?':
+            print_help();
+            exit(EXIT_FAILURE);
+            break;
+      }
+   }
+
+   for (int i = optind; i < argc; i++) {
+      theRest        += argv[i];
+   }
+
+   // Get the piped commands
+   std::vector<std::string> piped_commands;
+   if(!isatty(STDIN_FILENO)) {
+      std::cout << "Reading piped commands...\n";
+      std::string lineInput;
+      while(std::getline(std::cin,lineInput)) {
+         piped_commands.push_back(lineInput);
+      }
+   }
 
   auto reader = podio::ROOTReader();
   auto store  = podio::EventStore();
@@ -369,3 +454,85 @@ int main(){
 
   return 0;
 }
+
+//______________________________________________________________________________
+
+std::string exec(const char* cmd) {
+   std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+   if (!pipe) return "ERROR";
+   char buffer[128];
+   std::string result = "";
+   while (!feof(pipe.get())) {
+      if (fgets(buffer, 128, pipe.get()) != NULL)
+         result += buffer;
+   }
+   return result;
+}
+//______________________________________________________________________________
+
+bool fexists(const std::string& filename) {
+   std::ifstream ifile(filename.c_str());
+   if( ifile ) return true;
+   return false;
+}
+//______________________________________________________________________________
+
+
+void check_field_maps()
+{
+   //wget http://clasweb.jlab.org/12gev/field_maps/clas12SolenoidFieldMap.dat
+   //wget http://clasweb.jlab.org/12gev/field_maps/clas12TorusOriginalMap.dat
+   //bool failed = false;
+
+   //if( ! fexists(C12SIM_DATA_DIR"/clas12SolenoidFieldMap.dat") ) {
+   //   std::cerr << "Error: Solenoid field map missing!" << std::endl;
+   //   std::cerr << "file \"" C12SIM_DATA_DIR "/clas12SolenoidFieldMap.dat\" not found. " << std::endl;
+   //   failed = true;
+   //}
+   //if( ! fexists(C12SIM_DATA_DIR"/clas12TorusOriginalMap.dat") ) {
+   //   std::cerr << "Error: Torus field map missing!" << std::endl;
+   //   std::cerr << "file \"" C12SIM_DATA_DIR "/clas12TorusOriginalMap.dat\" not found. " << std::endl;
+   //   failed = true;
+   //}
+   //if(failed) {
+   //   std::cerr << "Use \"c12sim --dl-field-maps\" to download and install the field maps" << std::endl;
+   //   exit(0);
+   //}
+}
+
+void download_field_maps()
+{
+   //wget http://clasweb.jlab.org/12gev/field_maps/clas12SolenoidFieldMap.dat
+   //wget http://clasweb.jlab.org/12gev/field_maps/clas12TorusOriginalMap.dat
+
+   //std::cout << "c12sim field map directory : " C12SIM_DATA_DIR << std::endl;
+   //if( ! fexists(C12SIM_DATA_DIR "/clas12SolenoidFieldMap.dat") ) {
+   //   std::cout << " Downloading  http://clasweb.jlab.org/12gev/field_maps/clas12SolenoidFieldMap.dat" << std::endl;
+   //   exec(" cd " C12SIM_DATA_DIR " ; wget http://clasweb.jlab.org/12gev/field_maps/clas12SolenoidFieldMap.dat && pwd && ls -lrth ");
+   //}
+   //if( ! fexists(C12SIM_DATA_DIR "/clas12TorusOriginalMap.dat") ) {
+   //   std::cout << " Downloading  http://clasweb.jlab.org/12gev/field_maps/clas12TorusOriginalMap.dat" << std::endl;
+   //   exec(" cd " C12SIM_DATA_DIR " ; wget http://clasweb.jlab.org/12gev/field_maps/clas12TorusOriginalMap.dat && pwd && ls -lrth ");
+   //}
+}
+//______________________________________________________________________________
+
+void print_help() {
+
+   std::cout << "usage: lcio2legacy [options] file.root    \n";
+   std::cout << "       Converts podio based lcio2 data into legacy-lcio format.\n";
+   std::cout << "Options:                               \n";
+   std::cout << "    -r, --run=NUMBER         Set simulation \"run\" NUMBER\n";
+   std::cout << "    -i, --input=FILE         Set the input file from which events will be read\n";
+   std::cout << "    -o, --output=NAME        Set the output file name which will have the run number appended\n";
+   std::cout << "                             Default is \"clas12sim\". Note this is just the file basename; use -D to set the directory \n";
+   std::cout << "    -n, --events=#           Causes the execution of the ui command \"/run/beamOn #\".\n";
+   std::cout << "    -O, --offset=#           Starting event offset.\n";
+   std::cout << "    -D, --dir=NAME           Set the output directory. The default is \"data/rootfiles/\"\n";
+   std::cout << "    -t, --treename=NAME      Set the output tree name. The default is \"clasdigi_hits\"\n";
+   //   {"solenoid-field", required_argument,  0, 'S'},
+   //   {"toroid-field", required_argument,  0, 'T'},
+   //   {"torus-field", required_argument,  0, 'T'},
+}
+//______________________________________________________________________________
+
